4329414fcb59da727612d4026fdba60768a982e0
IldarYakup <IldarYakup@gmail.com>
2021-10-25 14:50:23 +0300

    Board

--- CorridorGame/CorridorMain.py
+++ CorridorGame/CorridorMain.py
@@ -1,7 +1,7 @@
 import pygame as p
 from corridor.CorridorGame import CorridorEngine
 
-width = heigth = 400
+width = heigth = 512
 dimension = 17
 size = heigth // dimension
 max_fps = 15
@@ -9,7 +9,7 @@ images = {}
 
 
 def LoadImages():
-    pieces = ["Pl1", "Pl2"]
+    pieces = ["Pl1", "Pl2", "Bl", "Vl", "Gl", "Cross"]
     for piece in pieces:
         images[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (size, size))
 
@@ -27,29 +27,15 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-        drawGameState(screen, gs)
+        drawPieces(screen, gs.board)
         clock.tick(max_fps)
         p.display.flip()
-
-def drawGameState(screen, gs):
-    drawBoard(screen)
-    drawPieces(screen, gs.board)
-
-
-def drawBoard(screen):
-    colors = [p.Color('White'), p.Color('gray')]
-    for r in range(dimension):
-        for c in range(dimension):
-            color = colors[((r + c) % 2)]
-            p.draw.rect(screen, color, p.Rect(c * size, r * size, size, size))
-
 
 def drawPieces(screen, board):
     for r in range(dimension):
         for c in range(dimension):
             piece = board[r][c]
-            if piece != "." and piece != '|' and piece != '-' and piece != ' ':
-                screen.blit(images[piece], p.Rect(c * size, r * size, size, size))
+            screen.blit(images[piece], p.Rect(c * size, r * size, size, size))
 
 
 if __name__ == "__main__":
